a090093c22e5be99218639c9af1899c6ce22ba4d
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-10 09:39:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
- apiKey:"AIzaSyAE7SJVhS-FLBueWNAQxYA6Gi838YN55wU"
+  apiKey: "AIzaSyAE7SJVhS-FLBueWNAQxYA6Gi838YN55wU",
   authDomain: "gustebook-aba1d.firebaseapp.com",
-
-  projectId: "gustebook-aba1d", storageBucket:"gustebook-aba1d.firebasestorage.app",
-  messagingSenderId: "282519660063", appId:"1:732132267362:web:7b0692476e9ed89d1dc83b",
+  projectId: "gustebook-aba1d",
+  storageBucket: "gustebook-aba1d.firebasestorage.app",
+  messagingSenderId: "282519660063",
+  appId: "1:282519660063:web:d0ebdb62917160d4f6d72a",
+  measurementId: "G-15H56JYDZ0"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 setInterval(() => {
   const cursors = document.querySelectorAll('[style*="color: #0f0;"]');
