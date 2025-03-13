205903f695afb1cb967bfb8975dbaec52ef44c1a
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-13 16:36:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAE7SJVhS-FLBueWNAQxYA6Gi838YN55wU",
-  authDomain: "gustebook-aba1d.firebaseapp.com",
-  projectId: "gustebook-aba1d",
-  storageBucket: "gustebook-aba1d.firebasestorage.app",
-  messagingSenderId: "282519660063",
-  appId: "1:282519660063:web:d0ebdb62917160d4f6d72a",
-  measurementId: "G-15H56JYDZ0"
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
